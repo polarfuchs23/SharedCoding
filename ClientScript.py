1359a36a3a8534b3050ac52d3cf1e513215e0fd3
burg113 <70021977+burg113@users.noreply.github.com>
2020-10-30 19:36:45 +0100

    Add files via upload

--- ClientScript.py
+++ ClientScript.py
@@ -3,24 +3,27 @@ import selectors
 import time
 
 
+def send(content):
+    global sock
+    sock.send((str(len(content))+content).encode("utf-8"))
+
+
 sel = selectors.DefaultSelector()
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip = "mineburg.firewall-gateway.com"
 #ip = "192.168.178.40"
 
 
-print(s.connect_ex((ip, 5000)))
-tosend="Data"
-s.send((str(len(tosend))+tosend).encode("utf-8"))
-print("received:  ",s.recv(1024).decode("ascii"))
+print(sock.connect_ex((ip, 5000)))
+send("Data")
+print("received:  ",sock.recv(1024).decode("ascii"))
 
 time.sleep(2)
 
 while True:
     time.sleep(0.4)
-    tosend=1000*"---Test---"
-    s.send((str(len(tosend))+tosend).encode("utf-8"))
-    s.recv(40240000).decode("ascii")
+    send(1000*"---Test---")
+    #sock.recv(40240000).decode("ascii")
     print("runs")
-print("received:  ",s.recv(40240000).decode("ascii"))
-print("received:  ",s.recv(1024).decode("ascii"))
+    print("received:  ",sock.recv(40240000).decode("ascii"))
+print("received:  ",sock.recv(1024).decode("ascii"))
