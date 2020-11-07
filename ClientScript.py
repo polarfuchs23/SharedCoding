513872fd27ca3c324a97aa3fd7664d59660cf994
burg113 <70021977+burg113@users.noreply.github.com>
2020-11-07 19:14:44 +0100

    Add files via upload

--- ClientScript.py
+++ ClientScript.py
@@ -19,6 +19,27 @@ def send(content, sock):
 def sendstring(content, sock):
     sock.send((str(len(content))+content).encode("utf-8"))
 
+def awaitdata(sock):
+    recievedlength = sock.recv(10).decode("ascii")  # Should be ready to read
+    print(recievedlength, " ", recievedlength.encode("utf-8"))
+    length = ""
+    recv_data = b''
+    for s in recievedlength:
+        if s.isdigit():
+            length += s
+        else:
+            recv_data+=s.encode("utf-8")
+
+    print(length)
+    while len(recv_data)<int(length):
+        run = True
+        while run:
+            try:
+                recv_data += (sock.recv(int(length)))
+                run = False
+            except:
+                run = True
+    return recv_data;
 
 sel = selectors.DefaultSelector()
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,29 +61,7 @@ while True:
         break
     sendstring("a", sock)
     #sock.recv(40240000).decode("ascii")
-    print("runs")
-    print("received:  ",sock.recv(40240000).decode("ascii"))
+    print("received:  ", awaitdata(sock))
 print("received:  ", sock.recv(1024).decode("ascii"))
 
 
-def awaitdata(sock):
-    recievedlength = sock.recv(10).decode("ascii")  # Should be ready to read
-    print(recievedlength, " ", recievedlength.encode("utf-8"))
-    length = ""
-    recv_data = b''
-    for s in recievedlength:
-        if s.isdigit():
-            length+=s
-        else:
-            recv_data+=s.encode("utf-8")
-
-    print(length)
-    t1=time.time();
-    while len(recv_data)<int(length):
-        run=True
-        while run:
-            try:
-                recv_data += (sock.recv(int(length)))
-                run = False
-            except:
-                run = True
