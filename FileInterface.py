1bd8ab0c485ce09d8c5227cc18864664abc598f0
burg113 <70021977+burg113@users.noreply.github.com>
2020-11-20 11:58:49 +0100

    Add files via upload

---
+++ FileInterface.py
@@ -0,0 +1,37 @@
+import math
+
+def appendFile(name,string):
+    with open(name,'a') as file:
+        file.writelines(string)
+
+def writeFile(name,string):
+    with open(name,'w') as file:
+        file.writelines(string)
+
+
+def readFile(name):
+    with open(name,'r') as file:
+        return file.read()
+
+def readFileHex(name):
+    with open(name,'rb') as file:
+        return file.read().hex
+
+def readFileLines(name):
+    with open(name,'r') as file:
+        return file.read().split("\n")
+
+
+fileContent = readFile("file.test")
+
+writeFile("file2.test",fileContent)
+
+if fileContent == readFile("file.test"):
+    print("same")
+else:
+    print("different")
+
+
+
+
+
